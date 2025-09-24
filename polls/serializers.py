dd1dd43d5e6cfd5591ef37057a94ed8dd5ca2f0d
deenakamal <deenah.kamal@gmail.com>
2025-09-25 01:34:56 +0300

    feat:Added Poll and Option serializers to structure API responses.

--- polls/serializers.py
+++ polls/serializers.py
@@ -18,3 +18,35 @@ class VoteSerializer(serializers.ModelSerializer):
         model = Vote
         fields = ['id', 'poll', 'option', 'user']
         read_only_fields = ['user']
+        
+
+# -----------------------------------
+# Option serializer محسوبة runtime
+# -----------------------------------
+class OptionUserSerializer(serializers.ModelSerializer):
+    user_voted = serializers.SerializerMethodField()
+    percentage = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Option
+        fields = ['id', 'text', 'votes_count', 'user_voted', 'percentage']
+
+    def get_user_voted(self, obj):
+        user = self.context['request'].user
+        return obj.votes.filter(user=user).exists()
+
+    def get_percentage(self, obj):
+        total_votes = sum(option.votes_count for option in obj.poll.options.all())
+        if total_votes == 0:
+            return 0
+        return round((obj.votes_count / total_votes) * 100, 2)
+
+# -----------------------------------
+# Poll serializer مع الحقول الجديدة
+# -----------------------------------
+class PollUserVoteSerializer(serializers.ModelSerializer):
+    options = OptionUserSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Poll
+        fields = ['id', 'question', 'created_at', 'expires_at', 'options']
