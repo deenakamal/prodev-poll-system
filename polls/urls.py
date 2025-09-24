b26f433d519b7b210202cdaf9011c4155e91bbde
deenakamal <deenah.kamal@gmail.com>
2025-09-25 01:36:00 +0300

    feat:Added logic in URLs to route API endpoints to the corresponding views.

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PollListView, VoteCreateView
+from .views import PollListView, VoteCreateView, UserVotedPollsListView
 
 
 urlpatterns = [
     path('', PollListView.as_view(), name='poll-list'),
     path('vote/', VoteCreateView.as_view(), name='vote-create'),
+      path('my-votes/', UserVotedPollsListView.as_view(), name='user-voted-polls'),
     
 ]
