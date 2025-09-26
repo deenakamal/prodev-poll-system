a53a3893b79af2c3fc4de7b41abb58e995992203
deenakamal <deenah.kamal@gmail.com>
2025-09-26 03:39:23 +0300

    resgister user, delete

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """
+    Custom admin panel for User model with UUID primary key.
+    Extends the default Django UserAdmin.
+    """
+    # Fields shown in the list page
+    list_display = ("id", "username", "email", "first_name", "last_name", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active", "is_superuser")
+    search_fields = ("username", "email", "first_name", "last_name")
+    ordering = ("username",)
+
+    # Define fieldsets for the edit user page
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    # Define fields for creating a new user in the admin
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "is_active", "is_staff"),
+        }),
+    )
