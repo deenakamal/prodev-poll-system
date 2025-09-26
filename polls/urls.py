bedcf10e10912ac517163f0495632ae9feb4e9d6
deenakamal <deenah.kamal@gmail.com>
2025-09-26 03:41:49 +0300

    make sure every files updated

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import PollListView, VoteCreateView, UserVotedPollsListView
+from .views import PollListView, VoteCreateView, UserVotedPollsListView, PollResultsView
 
 
 urlpatterns = [
     path('', PollListView.as_view(), name='poll-list'),
     path('vote/', VoteCreateView.as_view(), name='vote-create'),
-      path('my-votes/', UserVotedPollsListView.as_view(), name='user-voted-polls'),
+    path('my-votes/', UserVotedPollsListView.as_view(), name='user-voted-polls'),
+    path('results/<int:pk>/', PollResultsView.as_view(), name='poll-results'),
+
     
 ]
